51558ed90acabbb2c3ecc246330503e5f617a916
musca <muscaestar@hotmail.com>
2020-07-11 22:06:07 +1000

    initial commit

---
+++ src/main/java/xyz/muscaestar/muscabaristaclient/MuscaBaristaClientApplication.java
@@ -0,0 +1,13 @@
+package xyz.muscaestar.muscabaristaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuscaBaristaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuscaBaristaClientApplication.class, args);
+    }
+
+}
